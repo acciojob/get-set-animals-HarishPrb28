ea77517bf6f655e7276c5bcdf8cda1179ad0c4bd
Harish  Parab <50201375+HarishPrb28@users.noreply.github.com>
2023-05-03 23:19:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 //complete this code
 class Animal {
-	constructor(species){
-		this.species = species;
-	}
-	getspecies(){
-		return this.species
-	}
-	makeSound(){
-		console.log(`The ${this.species} makes sound`)
-	}
-}
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {
-	bark(){
-		console.log('woof');
-	}
+  get species() {
+    return this._species;
+  }
+
+  makeSound() {
+    console.log(`The ${this._species} makes a sound.`);
+  }
 }
 
 class Cat extends Animal {
-	purr(){
-		console.log('purr');
-	}
+  purr() {
+    console.log('purr');
+  }
 }
+
+class Dog extends Animal {
+  bark() {
+    console.log('woof');
+  }
+}
+
 
 // Do not change the code below this line
 window.Animal = Animal;
